1972c7ae34070f3ccc5a84a19b63d5d31d519c09
Leon <leonivanrodrigo@gmail.com>
2019-07-24 22:53:46 -0300

    WIP. Landscape.

--- scripts/app.js
+++ scripts/app.js
@@ -1,40 +1,133 @@
 $(document).ready(function () {
-    $('.main').css('display','flex');
+    const fixture = info.matches;
+    const teams = info.teams;
+    const locations = info.locations;
 
+    displayMatches();
+    displayTeams();
+    displayLocations();
+
+    //Ir al id.
     $('.main a').click(function (e) {
-        $('.main').css('display','none');
         let aux = $(e.target);
         let proximaPagina;
-        if(typeof aux.parent()[0].hash !== "undefined") {
+        if (typeof aux.parent()[0].hash !== "undefined") {
             proximaPagina = aux.parent()[0].hash;
         } else {
             proximaPagina = e.target.hash;
         }
-         
-        // $('#main').removeClass('activo');
-        $(proximaPagina).addClass('activo');
-        $('#nav-bar').css('display','flex');
+        transition(proximaPagina);
+        $('#nav-bar').addClass('nav-activo');
 
     })
+    //Volver al menu inicial
     $('#logo-link').click(function (e) {
 
-        $('.pages .activo').removeClass('activo');
-        // $('#main').removeClass('activo');
-        $('#nav-bar').css('display','none');
-        $('.main').css('display','flex');
+        $('#nav-bar').removeClass('nav-activo');
+
+        transition('#index');
 
     })
     //Detectar proxima pagina y ocultar.
     $('#nav-bar a').click(function (e) {
         e.preventDefault();
         let aux = $(e.target);
-        if(typeof aux.parent()[0].hash !== "undefined") {
+        if (typeof aux.parent()[0].hash !== "undefined") {
             proximaPagina = aux.parent()[0].hash;
         } else {
             proximaPagina = e.target.hash;
         }
-        $('.pages .activo').removeClass('activo');
-        $(proximaPagina).addClass('activo');
+        transition(proximaPagina);
     })
+
+    function transition(toPage) {
+        var toPage = $(toPage);
+        var fromPage = $('.pages .activo');
+        if(toPage.hasClass('activo') || toPage === fromPage) {
+            return;
+        }
+        toPage
+            .addClass('activo fade in')
+            .one('webkitAnimationEnd animationend', function () {
+                fromPage.removeClass('activo fade out');
+                toPage.removeClass('fade in');
+            })
+        fromPage.addClass('fade out');
+    }
+    
+    function displayMatches(){
+
+        fixture.forEach(match => {
+            $('#fixture').append(`
+            <!-- 1er Partido -->
+            <div class="detalle-partido-container">
+                <a class="detalle-partido" href="#fixture-detalle">
+                    <div class="fixture-partido">
+                        <img class="img-club left" src="${match.team1img}" alt="">
+                        <p class="versus">${match.team1}</p>
+                        <p class="versus">-</p>
+                        <p class="versus">${match.team2}</p>
+                        <img class="img-club right" src="${match.team2img}" alt="">
+                    </div>
+                </a>
+            </div>
+            `)
+        })
+        
+    }
+    function displayTeams(){
+
+        teams.forEach(team => {
+            $('#equipos').append(`
+            <div class="equipos-item mt-3 mx-auto">
+                <span class="equipos-titulo">${team.team_name}</span>
+                <img class="equipos-img" src="${team.team_logo_img}" alt="${team.team_name}">
+            </div>
+            `)
+        })
+
+    }
+    function displayLocations(){
+
+        locations.forEach(location => {
+            $('#sedes .flex-column').append(`
+                          <div class="mt-3">
+
+            <div class="sedes-item mx-auto" data-toggle="modal" data-target="#${location.id}">
+                <span class="sedes-titulo">${location.name}</span>
+            </div>
+        
+            <div class="modal fade" id="${location.id}">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content ">
+        
+                        <!-- Modal Header -->
+                        <div class="modal-header">
+                            <div>
+                                <h4 class="modal-title">${location.name}</h4>
+                                <br>
+                                <h6>${location.address}</h6>
+                            </div>
+        
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        </div>
+        
+                        <!-- Modal body -->
+                        <div class="modal-body">
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <iframe class="border"
+                                    src="${location.map}"></iframe>
+                            </div>
+                        </div>
+        
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+            `)
+        })
+
+    }
 
 });
---
+++ scripts/info.js
@@ -0,0 +1,665 @@
+var info =
+{
+  "teams":[
+    {
+      "team_name": "DFC Academy",
+      "team_logo_img": "./img/equipos/dfca.png",
+      "team_position": "3",
+      "matches_played": "25",
+      "wins": "15",
+      "draws": "6",
+      "loses": "4",
+      "goal_difference":"24",
+      "points":"51",
+      "players":[
+        {
+          "name":"Luke Grant",
+          "position":"GK"
+        },
+        {
+          "name":"Harrison Ball",
+          "position":"DEF"
+        },
+        {
+          "name":"Oscar Davidson",
+          "position":"DEF"
+        },
+        {
+          "name":"Dexter Hudson",
+          "position":"DEF"
+        },
+        {
+          "name":"Bailey James",
+          "position":"DEF"
+        },
+        {
+          "name":"Jamie Pace",
+          "position":"MID"
+        },
+        {
+          "name":"Davis Randall",
+          "position":"MID"
+        },
+        {
+          "name":"Sawyer Avery",
+          "position":"MID"
+        },
+        {
+          "name":"Carl Newman",
+          "position":"FWD"
+        },
+        {
+          "name":"Paul Irwin",
+          "position":"FWD"
+        },
+        {
+          "name":"Todd Kent",
+          "position":"FWD"
+        },
+        {
+          "name":"Theo Murray",
+          "position":"Supplent"
+        }
+      ]
+    },
+    {
+      "team_name": "KTFA Club",
+      "team_logo_img": "./img/equipos/ktfa.png",
+      "team_position": "4",
+      "matches_played": "25",
+      "wins": "13",
+      "draws": "6",
+      "loses": "6",
+      "goal_difference":"21",
+      "points":"45",
+      "players":[
+        {
+          "name":"Joshua Morris",
+          "position":"GK"
+        },
+        {
+          "name":"Jayden Wallace",
+          "position":"DEF"
+        },
+        {
+          "name":"Michael Murray",
+          "position":"DEF"
+        },
+        {
+          "name":"Spencer Poole",
+          "position":"DEF"
+        },
+        {
+          "name":"Danny Floyd",
+          "position":"DEF"
+        },
+        {
+          "name":"Maximilian Floyd",
+          "position":"MID"
+        },
+        {
+          "name":"Owen Calderon",
+          "position":"MID"
+        },
+        {
+          "name":"Camron Head",
+          "position":"MID"
+        },
+        {
+          "name":"Tyrone Casey",
+          "position":"FWD"
+        },
+        {
+          "name":"Frankie Porter",
+          "position":"FWD"
+        },
+        {
+          "name":"Theo Moss",
+          "position":"FWD"
+        }
+      ]
+    },
+    {
+      "team_name": "Baroda FC",
+      "team_logo_img": "./img/equipos/baroda.png",
+      "team_position": "5",
+      "matches_played": "25",
+      "wins": "12",
+      "draws": "6",
+      "loses": "7",
+      "goal_difference":"7",
+      "points":"42",
+      "players":[
+        {
+          "name":"Tom Baxter",
+          "position":"GK"
+        },
+        {
+          "name":"Henry Cox",
+          "position":"DEF"
+        },
+        {
+          "name":"Edward Richards",
+          "position":"DEF"
+        },
+        {
+          "name":"Sam Hunter",
+          "position":"DEF"
+        },
+        {
+          "name":"Sage Tyson",
+          "position":"DEF"
+        },
+        {
+          "name":"Joaquin Mercer",
+          "position":"MID"
+        },
+        {
+          "name":"Freddie Brooks",
+          "position":"MID"
+        },
+        {
+          "name":"Louis Cooke",
+          "position":"MID"
+        },
+        {
+          "name":"Harley Gill",
+          "position":"FWD"
+        },
+        {
+          "name":"Nico Mccormick",
+          "position":"FWD"
+        },
+        {
+          "name":"Sam James",
+          "position":"FWD"
+        }
+      ]
+    },
+    {
+      "team_name": "Kinka FC",
+      "team_logo_img": "./img/equipos/kinka.png",
+      "team_position": "2",
+      "matches_played": "25",
+      "wins": "15",
+      "draws": "8",
+      "loses": "2",
+      "goal_difference":"15",
+      "points":"53",
+      "players":[
+        {
+          "name":"Willie Stuart",
+          "position":"GK"
+        },
+        {
+          "name":"Dexter Elliott",
+          "position":"DEF"
+        },
+        {
+          "name":"George Ellis",
+          "position":"DEF"
+        },
+        {
+          "name":"Jude Gibson",
+          "position":"DEF"
+        },
+        {
+          "name":"Franco Howell",
+          "position":"DEF"
+        },
+        {
+          "name":"Ariel Massey",
+          "position":"MID"
+        },
+        {
+          "name":"Jimmy Schroeder",
+          "position":"MID"
+        },
+        {
+          "name":"Corey Holmes",
+          "position":"MID"
+        },
+        {
+          "name":"Daniel Anderson",
+          "position":"FWD"
+        },
+        {
+          "name":"Bradley Lester",
+          "position":"FWD"
+        },
+        {
+          "name":"Brent Olson",
+          "position":"FWD"
+        }
+      ]
+    },
+    {
+      "team_name": "Tusla United",
+      "team_logo_img": "./img/equipos/tusla.png",
+      "team_position": "6",
+      "matches_played": "25",
+      "wins": "11",
+      "draws": "7",
+      "loses": "7",
+      "goal_difference":"9",
+      "points":"40",
+      "players":[
+        {
+          "name":"Mason Webb",
+          "position":"GK"
+        },
+        {
+          "name":"Jude Powell",
+          "position":"DEF"
+        },
+        {
+          "name":"Maximilian Mason",
+          "position":"DEF"
+        },
+        {
+          "name":"Curtis Rosario",
+          "position":"DEF"
+        },
+        {
+          "name":"Nicolas Stephenson",
+          "position":"DEF"
+        },
+        {
+          "name":"Christopher Barker",
+          "position":"MID"
+        },
+        {
+          "name":"Alexander Gordon",
+          "position":"MID"
+        },
+        {
+          "name":"Adrian Daugherty",
+          "position":"MID"
+        },
+        {
+          "name":"Kai Scott",
+          "position":"FWD"
+        },
+        {
+          "name":"Peter Miller",
+          "position":"FWD"
+        },
+        {
+          "name":"Finlay Harrison",
+          "position":"FWD"
+        }
+      ]
+    },
+    {
+      "team_name": "New York",
+      "team_logo_img": "./img/equipos/york.png",
+      "team_position": "1",
+      "matches_played": "25",
+      "wins": "17",
+      "draws": "6",
+      "loses": "2",
+      "goal_difference":"27",
+      "points":"57",
+      "players":[
+        {
+          "name":"Ben May",
+          "position":"GK"
+        },
+        {
+          "name":"Jacob Burns",
+          "position":"DEF"
+        },
+        {
+          "name":"Brady Mccray",
+          "position":"DEF"
+        },
+        {
+          "name":"Deshawn Fletcher",
+          "position":"DEF"
+        },
+        {
+          "name":"King Sheppard",
+          "position":"DEF"
+        },
+        {
+          "name":"Charlie Williamson",
+          "position":"MID"
+        },
+        {
+          "name":"Austyn Franklin",
+          "position":"MID"
+        },
+        {
+          "name":"Garrett Norton",
+          "position":"MID"
+        },
+        {
+          "name":"Bradley Pena",
+          "position":"FWD"
+        },
+        {
+          "name":"Archie Barnes",
+          "position":"FWD"
+        },
+        {
+          "name":"Luca Butler",
+          "position":"FWD"
+        }
+      ]
+    }
+  ],
+
+  "locations":[
+    {
+      "name":"AJ Katzenmaier Elementary",
+      "id":"ajk",
+      "address":"24 W. Walton St., Chicago, IL 60610",
+      "map":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.6542451446303!2d-87.6290504!3d41.9002924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34e07f6bac3%3A0x68a82e5d59952c86!2s24+W+Walton+St%2C+Chicago%2C+IL+60610%2C+EE.+UU.!5e0!3m2!1ses-419!2sar!4v1555370634413!5m2!1ses-419!2sar"
+    },
+    {
+      "name":"Greenbay Elementary",
+      "id":"gb",
+      "address":"1734 N. Orleans St., Chicago, IL 60614",
+      "map":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.0256986745426!2d-87.64002798505479!3d41.91380627115881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34073f306a3%3A0x9e1726bbf8f23f0e!2s1734+N+Orleans+St%2C+Chicago%2C+IL+60614%2C+EE.+UU.!5e0!3m2!1ses-419!2sar!4v1554989792517!5m2!1ses-419!2sar"
+    },
+    {
+      "name":"Howard A Yaeger Elementary",
+      "id":"hay",
+      "address":"2245 N. Southport Ave., Chicago, IL 60614",
+      "map":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.5854973846845!2d-87.66511458505448!3d41.92326857056615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f9b8d2d%3A0x62ad8b907dd755d6!2s2245+N+Southport+Ave%2C+Chicago%2C+IL+60614%2C+EE.+UU.!5e0!3m2!1ses-419!2sar!4v1554989915777!5m2!1ses-419!2sar"
+    },
+    {
+      "name":"Marjorie P Hart Elementary",
+      "id":"hart",
+      "address":"2625 N. Orchard St., Chicago, IL 60614",
+      "map":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.291728953083!2d-87.64808628505428!3d41.92958227017081!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd30f2630e551%3A0x3e719e44a5cef714!2s2625+N+Orchard+St%2C+Chicago%2C+IL+60614%2C+EE.+UU.!5e0!3m2!1ses-419!2sar!4v1554990385150!5m2!1ses-419!2sar"
+    },
+    {
+      "name":"North Elementary",
+      "id":"nrt",
+      "address":"1409 N. Ogden Ave., Chicago, IL 60610",
+      "map":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.337614159601!2d-87.64837698505504!3d41.907100471578765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd33af0e6ccc3%3A0x26c81c1d557667da!2s1409+N+Ogden+Ave%2C+Chicago%2C+IL+60610%2C+EE.+UU.!5e0!3m2!1ses-419!2sar!4v1554990460658!5m2!1ses-419!2sar"
+    },
+    {
+      "name":"South Elementary",
+      "id":"sth",
+      "address":"2101 N. Fremont St., Chicago, IL 60614",
+      "map":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.7477648265644!2d-87.65355538505459!3d41.91978077078475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3196fb41dc7%3A0x970be7f7d6336df5!2s2101+N+Fremont+St%2C+Chicago%2C+IL+60614%2C+EE.+UU.!5e0!3m2!1ses-419!2sar!4v1554990551021!5m2!1ses-419!2sar"
+    }
+  ],
+
+  "matches":[
+    {
+      "team1":"DFC Academy",
+      "team1img": "./img/equipos/dfca.png",
+      "team2":"Kinka FC",
+      "team2img": "./img/equipos/kinka.png",
+      "month":"September",
+      "monthN":"3",
+      "day":"01",
+      "fullDay":"September 1st",
+      "matchID":"01080930",
+      "location":"",
+      "location_id":"ajk",
+      "time":"9:30 a.m.",
+      "timeH":"09",
+      "timeM":"30"
+    },
+    {
+      "team1":"Baroda FC",
+      "team1img": "./img/equipos/baroda.png",
+      "team2":"Kinka FC",
+      "team2img": "./img/equipos/kinka.png",
+      "month":"September",
+      "monthN":"8",
+      "day":"01",
+      "fullDay":"September 1st",
+      "matchID":"01081300",
+      "location":"",
+      "location_id":"gb",
+      "time":"1:00 p.m.",
+      "timeH":"13",
+      "timeM":"00"
+    },
+    {
+      "team1":"Tusla United",
+      "team1img": "./img/equipos/tusla.png",
+      "team2":"New York",
+      "team2img": "./img/equipos/york.png",
+      "month":"September",
+      "monthN":"8",
+      "day":"08",
+      "fullDay":"September 8th",
+      "matchID":"08080930",
+      "location":"",
+      "location_id":"hay",
+      "time":"9:30 a.m.",
+      "timeH":"09",
+      "timeM":"30"
+    },
+    {
+      "team1":"New York",
+      "team1img": "./img/equipos/york.png",
+      "team2":"DFC Academy",
+      "team2img": "./img/equipos/dfca.png",
+      "month":"September",
+      "monthN":"8",
+      "day":"08",
+      "fullDay":"September 8th",
+      "matchID":"08081300",
+      "location":"",
+      "location_id":"hart",
+      "time":"1:00 p.m.",
+      "timeH":"13",
+      "timeM":"00"
+    },
+    {
+      "team1":"KTFA Club",
+      "team1img": "./img/equipos/ktfa.png",
+      "team2":"Kinka FC",
+      "team2img": "./img/equipos/kinka.png",
+      "month":"September",
+      "monthN":"8",
+      "day":"15",
+      "fullDay":"September 15th",
+      "matchID":"15080930",
+      "location":"",
+      "location_id":"nrt",
+      "time":"9:30 a.m.",
+      "timeH":"09",
+      "timeM":"30"
+    },
+    {
+      "team1":"Baroda FC",
+      "team1img": "./img/equipos/baroda.png",
+      "team2":"Tusla United",
+      "team2img": "./img/equipos/tusla.png",
+      "month":"September",
+      "monthN":"8",
+      "day":"15",
+      "fullDay":"September 15th",
+      "matchID":"15081300",
+      "location":"",
+      "location_id":"ajk",
+      "time":"1:00 p.m.",
+      "timeH":"13",
+      "timeM":"00"
+    },
+    {
+      "team1":"DFC Academy",
+      "team1img": "./img/equipos/dfca.png",
+      "team2":"Baroda FC",
+      "team2img": "./img/equipos/baroda.png",
+      "month":"September",
+      "monthN":"8",
+      "day":"22",
+      "fullDay":"September 22nd",
+      "matchID":"22080930",
+      "location":"",
+      "location_id":"sth",
+      "time":"9:30 a.m.",
+      "timeH":"09",
+      "timeM":"30"
+    },
+    {
+      "team1":"KTFA Club",
+      "team1img": "./img/equipos/ktfa.png",
+      "team2":"New York",
+      "team2img": "./img/equipos/york.png",
+      "month":"September",
+      "monthN":"8",
+      "day":"22",
+      "fullDay":"September 22nd",
+      "matchID":"22081300",
+      "location":"",
+      "location_id":"hay",
+      "time":"1:00 p.m.",
+      "timeH":"13",
+      "timeM":"00"
+    },
+    {
+      "team1":"Kinka FC",
+      "team1img": "./img/equipos/kinka.png",
+      "team2":"Tusla United",
+      "team2img": "./img/equipos/tusla.png",
+      "month":"September",
+      "monthN":"8",
+      "day":"29",
+      "fullDay":"September 29th",
+      "matchID":"29080930",
+      "location":"",
+      "location_id":"gb",
+      "time":"9:30 a.m.",
+      "timeH":"09",
+      "timeM":"30"
+    },
+    {
+      "team1":"KTFA Club",
+      "team1img": "./img/equipos/ktfa.png",
+      "team2":"Tusla United",
+      "team2img": "./img/equipos/tusla.png",
+      "month":"October",
+      "monthN":"9",
+      "day":"06",
+      "fullDay":"October 6th",
+      "matchID":"06090930",
+      "location":"",
+      "location_id":"hart",
+      "time":"9:30 a.m.",
+      "timeH":"09",
+      "timeM":"30"
+    },
+    {
+      "team1":"DFC Academy",
+      "team1img": "./img/equipos/dfca.png",
+      "team2":"New York",
+      "team2img": "./img/equipos/york.png",
+      "month":"October",
+      "monthN":"9",
+      "day":"06",
+      "fullDay":"October 6th",
+      "matchID":"06091300",
+      "location":"",
+      "location_id":"sth",
+      "time":"1:00 p.m.",
+      "timeH":"13",
+      "timeM":"00"
+    },
+    {
+      "team1":"Baroda FC",
+      "team1img": "./img/equipos/baroda.png",
+      "team2":"Kinka FC",
+      "team2img": "./img/equipos/kinka.png",
+      "month":"October",
+      "monthN":"9",
+      "day":"13",
+      "fullDay":"October 13rd",
+      "matchID":"13090930",
+      "location":"",
+      "location_id":"hay",
+      "time":"9:30 a.m.",
+      "timeH":"09",
+      "timeM":"30"
+    },
+    {
+      "team1":"Tusla United",
+      "team1img": "./img/equipos/tusla.png",
+      "team2":"DFC Academy",
+      "team2img": "./img/equipos/dfca.png",
+      "month":"October",
+      "monthN":"9",
+      "day":"13",
+      "fullDay":"October 13rd",
+      "matchID":"13091300",
+      "location":"",
+      "location_id":"gb",
+      "time":"1:00 p.m.",
+      "timeH":"13",
+      "timeM":"00"
+    },
+    {
+      "team1":"New York",
+      "team1img": "./img/equipos/york.png",
+      "team2":"Baroda FC",
+      "team2img": "./img/equipos/baroda.png",
+      "month":"October",
+      "monthN":"9",
+      "day":"20",
+      "fullDay":"October 20th",
+      "matchID":"20090930",
+      "location":"",
+      "location_id":"nrt",
+      "time":"9:30 a.m.",
+      "timeH":"09",
+      "timeM":"30"
+    },
+    {
+      "team1":"KTFA Club",
+      "team1img": "./img/equipos/ktfa.png",
+      "team2":"Kinka FC",
+      "team2img": "./img/equipos/kinka.png",
+      "month":"October",
+      "monthN":"9",
+      "day":"20",
+      "fullDay":"October 20th",
+      "matchID":"20091300",
+      "location":"",
+      "location_id":"hart",
+      "time":"1:00 p.m.",
+      "timeH":"13",
+      "timeM":"00"
+    },
+    {
+      "team1":"Baroda FC",
+      "team1img": "./img/equipos/baroda.png",
+      "team2":"DFC Academy",
+      "team2img": "./img/equipos/dfca.png",
+      "month":"October",
+      "monthN":"9",
+      "day":"27",
+      "fullDay":"October 27th",
+      "matchID":"27090930",
+      "location":"",
+      "location_id":"ajk",
+      "time":"9:30 a.m.",
+      "timeH":"09",
+      "timeM":"30"
+    },
+    {
+      "team1":"Tusla United",
+      "team1img": "./img/equipos/tusla.png",
+      "team2":"New York",
+      "team2img": "./img/equipos/york.png",
+      "month":"October",
+      "monthN":"9",
+      "day":"27",
+      "fullDay":"October 27th",
+      "matchID":"27091300",
+      "location":"",
+      "location_id":"hay",
+      "time":"1:00 p.m.",
+      "timeH":"13",
+      "timeM":"00"
+    }
+  ]
+}
