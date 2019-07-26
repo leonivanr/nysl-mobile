8eb7b0c54ae2a16f177f7097193868dc81e51930
Leon <leonivanrodrigo@gmail.com>
2019-07-26 03:22:34 -0300

    Fin fixture.

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
     displayMatches();
     displayTeams();
     displayLocations();
-
     //Ir al id.
     $('.main a').click(function (e) {
         let aux = $(e.target);
@@ -23,9 +22,8 @@ $(document).ready(function () {
     //Volver al menu inicial
     $('#logo-link').click(function (e) {
 
-        $('#nav-bar').removeClass('nav-activo');
-
         transition('#index');
+        $('#nav-bar').removeClass('nav-activo');
 
     })
     //Detectar proxima pagina y ocultar.
@@ -39,11 +37,43 @@ $(document).ready(function () {
         }
         transition(proximaPagina);
     })
+    $(document).on("click", ".detalle-partido", function () {
+        let matchId = $(this).attr('id');
+        let resultadoMatch = fixture.filter(x => x.matchID === matchId);
+        let locationId = resultadoMatch[0].location_id;
+        let locationMatch = locations.filter(x => x.id === locationId);
+
+        $("#body-data").html(
+            `
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <br>
+            <div class="equipo-detalle">
+                <div class="text-center">
+                    <p class="versus-modal">${resultadoMatch[0].team1}</p>
+                    <img class="img-club-detalle" src="${resultadoMatch[0].team1img}" alt="">
+                </div>
+                <p class="versus-modal">VS</p>
+                <div class="text-center">
+                    <p class="versus-modal">${resultadoMatch[0].team2}</p>
+                    <img class="img-club-detalle" src="${resultadoMatch[0].team2img}" alt="">
+                </div>
+            </div>
+            <hr>
+            <div class="equipo-info mt-1">
+                <h5>Hora: <span class="modal-date">${resultadoMatch[0].time}</span></h5>
+                <h5>Fecha: <span class="modal-date">${resultadoMatch[0].fullDay}</span></h5>
+                <h5>Lugar: <span class="modal-date">${locationMatch[0].name}</span></h5>
+                <h5>Dirección: <span class="modal-date">${locationMatch[0].address}</span></h5>
+                <iframe class="border" src="${locationMatch[0].map}"></iframe>
+            </div>
+            `
+        )
+    });
 
     function transition(toPage) {
         var toPage = $(toPage);
         var fromPage = $('.pages .activo');
-        if(toPage.hasClass('activo') || toPage === fromPage) {
+        if (toPage.hasClass('activo') || toPage === fromPage) {
             return;
         }
         toPage
@@ -54,28 +84,41 @@ $(document).ready(function () {
             })
         fromPage.addClass('fade out');
     }
-    
-    function displayMatches(){
 
-        fixture.forEach(match => {
-            $('#fixture').append(`
-            <!-- 1er Partido -->
-            <div class="detalle-partido-container">
-                <a class="detalle-partido" href="#fixture-detalle">
-                    <div class="fixture-partido">
-                        <img class="img-club left" src="${match.team1img}" alt="">
-                        <p class="versus">${match.team1}</p>
-                        <p class="versus">-</p>
-                        <p class="versus">${match.team2}</p>
-                        <img class="img-club right" src="${match.team2img}" alt="">
-                    </div>
-                </a>
-            </div>
-            `)
+    function displayMatches() {
+
+        fixture.filter(x => x.fullDay === "September 1st").forEach(match => {
+            $('#domingoUno').append(showM(match));
         })
-        
+        fixture.filter(x => x.fullDay === "September 8th").forEach(match => {
+            $('#domingoOcho').append(showM(match));
+        })
+        fixture.filter(x => x.fullDay === "September 15th").forEach(match => {
+            $('#domingoQuince').append(showM(match));
+        })
+        fixture.filter(x => x.fullDay === "September 22nd").forEach(match => {
+            $('#domingoVdos').append(showM(match));
+        })
+        fixture.filter(x => x.fullDay === "September 29th").forEach(match => {
+            $('#domingoVnueve').append(showM(match));
+        })
+        fixture.filter(x => x.fullDay === "October 6th").forEach(match => {
+            $('#domingoSeis').append(showM(match));
+        })
+        fixture.filter(x => x.fullDay === "October 13rd").forEach(match => {
+            $('#domingoTrece').append(showM(match));
+        })
+        fixture.filter(x => x.fullDay === "October 20th").forEach(match => {
+            $('#domingoVeinte').append(showM(match));
+        })
+        fixture.filter(x => x.fullDay === "October 27th").forEach(match => {
+            $('#domingoVsiete').append(showM(match));
+        })
+
+
     }
-    function displayTeams(){
+
+    function displayTeams() {
 
         teams.forEach(team => {
             $('#equipos').append(`
@@ -87,7 +130,8 @@ $(document).ready(function () {
         })
 
     }
-    function displayLocations(){
+
+    function displayLocations() {
 
         locations.forEach(location => {
             $('#sedes .flex-column').append(`
@@ -130,4 +174,20 @@ $(document).ready(function () {
 
     }
 
+    function showM(match) {
+        return `
+        <!-- 1er Partido -->
+        <div  class="detalle-partido-container" >
+            <a id="${match.matchID}" class="detalle-partido" href="#fixture-detalle" data-toggle="modal" data-target="#myModal">
+                <div class="fixture-partido">
+                    <img class="img-club left" src="${match.team1img}" alt="">
+                    <p class="versus">${match.team1}</p>
+                    <p class="versus">-</p>
+                    <p class="versus">${match.team2}</p>
+                    <img class="img-club right" src="${match.team2img}" alt="">
+                </div>
+            </a>
+        </div>
+        `
+    }
 });
